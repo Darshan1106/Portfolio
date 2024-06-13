6e44cb4b741cf48854c6526a4373881cf3bed860
Darshan Deshmukh <107185415+Darshan1233@users.noreply.github.com>
2024-06-13 10:55:18 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,343 @@
+export const Bio = {
+  name: "Darshan Deshmukh",
+  roles: [
+    "Software Engineer",
+    "Full Stack Developer",
+    "Technical Enthusiast",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Darshan1233",
+  resume:
+    "https://drive.google.com/file/d/1YQDGhyev7sOxDzql1UO4Y6kDTDfln4Xz/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/darshan-deshmukh11",
+  twitter: "",
+  insta: "https://www.instagram.com/darshan_deshmukh_11/",
+  facebook: "",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://mui.com/static/logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://www.rapidbrains.com/assets/img/services/rapidbrains-expressjs.webp",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://cdn-icons-png.flaticon.com/512/136/136526.png",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "ChatGPT",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/2048px-ChatGPT_logo.svg.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQQW2B-KrHn-wJyEQz7VsGVMWRO0LtZsspEw&s",
+    role: "Full Stack Developer Intern",
+    company: "Emerging Technologies Software & Automation Solutions",
+    date: "Dec 2023 - Jan 2024",
+    desc: "Working on C# .NET as Frontend and Python as Backend.",
+    skills: [
+      "C#",
+      "ASP .NET",
+      "FireStore",
+      "Desktop Application",
+      "Python",
+      "Image Processing",
+      "Flask",
+      "GitHub",
+    ],
+    doc: "https://github.com/Darshan1233/Certificates/assets/107185415/edb98132-19fe-4a89-8ce5-a3253403c0cc",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLIN051wborYpGuk2YjnH5ruJz27W2yZEN0w&s",
+    role: "Web Development Intern",
+    company: "Bharat Intern",
+    date: "July 2023 - Aug 2023",
+    desc: "Working on the frontend of the web application using HTML, CSS & JavaScript.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://github.com/Darshan1233/Certificates/assets/107185415/2fa3bc06-7704-4292-adca-d005432bdcf0",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuXKx_MuIDsTPUs5H0W8gUDvoPS4VPKF3iTA&s",
+    role: "Web Development Intern",
+    company: "Sync Intern",
+    date: "July 2023 - Aug 2023",
+    desc: "Built an E-Learning full stack web app used Bootstrap, HTML, PHP & MySQL.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "PHP",
+      "MySQL",
+      "XAMPP",
+    ],
+    doc: "https://github.com/Darshan1233/Certificates/assets/107185415/78e6ba33-90ca-4160-9311-0f31f8b83d43",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://in.mathworks.com/academia/tah-portal/k-k-wagh-institute-of-engineering-education-and-research-40605349/_jcr_content/schoolLogo.adapt.full.medium.png/1661784924530.png",
+    school: "K K Wagh Institute of Engineering Education & Research, Nashik",
+    date: "Dec 2021 - June 2025",
+    grade: "9.05 CGPA",
+    desc: "Currently pursuing my BE in IT, I've been actively involved in honing my technical skills and collaborating on various projects.",
+    degree: "Bachelor of Engineering - BE, Information Technology",
+  },
+  {
+    id: 1,
+    img: "https://cbccollege.in/wp-content/uploads/2018/09/logo-150x150.png",
+    school: "R.N.C. Arts, J.D.B. Commerce , N.S.C. Science College, Nashik Road",
+    date: "June 2019 - June 2021",
+    grade: "91%",
+    desc: "I completed my class 12 high school education at R.N.C. Arts, J.D.B. Commerce , N.S.C. Science College, Nashik Road in Science with Electronic.",
+    degree: "HSC(XII), Science with Electronics",
+  },
+  {
+    id: 2,
+    img: "https://i.ytimg.com/vi/VSXclonV8X4/maxresdefault.jpg",
+    school: "Deolali High School, Deolali Camp",
+    date: "March 2018",
+    grade: "85%",
+    desc: "I completed my class 10 education at Deolali High School, Deolali Camp.",
+    degree: "SSC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "HotStar-Clone",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Just wrapped up a project showcasing my frontend skills! I recreated Hotstar's look and feel using only HTML, CSS, and JavaScript. Excited about future projects and exploring more frontend adventures! #FrontendDev #HotstarClone #HTMLCSSJS #WebDesign",
+    image:
+      "https://github.com/Darshan1233/Hotstar-Clone/assets/107185415/ee848280-f117-4d91-bba5-61829134ba54",
+    tags: [
+      "HTML",
+      "CSS",
+      "JAVASCRIPT",
+      "GITHUB",
+    ],
+    category: "web app",
+    github: "https://github.com/Darshan1233/Hotstar-Clone",
+    webapp: "https://darshan1106.github.io/HotStar-Clone/",
+  },
+  {
+    id: 1,
+    title: "Spotify-Clone",
+    date: "Sep 2023 - Oct 2023",
+    description:
+      "I recently completed a comprehensive Spotify clone website using the MERN stack (MongoDB, Express.js, React.js, Node.js). This project features user authentication with JWT tokens, encrypted password storage with bcrypt, and seamless API integration. Backend Technologies: Express.js, MongoDB (Mongoose ODM), JWT for authentication, Passport.js for authorization. Frontend Technologies: React.js, React Router for navigation, Tailwind CSS for styling, Cloudinary for media management, Howler.js for audio playback. This project demonstrates my proficiency in full-stack development, API integration, and creating immersive user experiences. Check it out and let me know your thoughts!",
+    image:
+      "https://github.com/Darshan1233/Spotify-Clone/assets/107185415/29c83da2-5925-4e6c-a7f6-691781dab17c",
+    tags: ["React Js", "MongoDB", "Node Js", "Express Js", "Tailwind CSS", "Cloudinary",],
+    category: "web app",
+    github: "https://github.com/Darshan1233/Spotify-Clone",
+    webapp: "https://github.com/Darshan1233/Spotify-Clone",
+    member: [
+    ],
+  },
+  {
+    id: 2,
+    title: "K-Map Solver",
+    date: "Nov 2022 - Jan 2023",
+    description:
+      "Developed a web-based Karnaugh Map (K-Map) solver using JavaScript and p5.js library. The application allows users to input truth tables, visualize K-Maps, and simplifies boolean functions through grouping and logical operations. Implemented features include interactive cell updates, Boolean function simplification, and dynamic visualizations of the K-Map.",
+    image:
+      "https://github.com/Darshan1233/K-Map/assets/107185415/2b041c41-b943-4697-a47e-164141ea9abe",
+    tags: [
+      "HTML",
+      "CSS",
+      "JAVASCRIPT",
+      "p5.js Library",
+      "Bootstrap",
+    ],
+    category: "web app",
+    github: "https://github.com/Darshan1233/K-Map",
+    webapp: "https://darshan1106.github.io/K-Map/",
+  },
+  {
+    id: 3,
+    title: "Human Activity Recognition with Smartphones",
+    date: "April 2024 - May 2024",
+    description:
+      "This project focuses on the use of smartphone sensor data to accurately classify and recognize different human activities. By leveraging the accelerometer and gyroscope data collected from smartphones, the project aims to build a robust machine learning model that can distinguish between activities such as walking, sitting, standing, laying down, and more dynamic activities like walking upstairs or downstairs. The successful completion of this project will result in an application capable of real-time human activity recognition using smartphone sensors. This has potential applications in health monitoring, fitness tracking, and enhancing user experiences in mobile applications.",
+    image:
+      "https://github.com/Darshan1233/Human-Activity-Recognition-with-Smartphones/assets/107185415/7779aa6a-317c-4113-82f5-71176409bba4",
+    tags: ["Python", "Machine Learning", "Data Science", "Big Data Analysis",],
+    category: "machine learning",
+    github: "https://github.com/Darshan1233/Human-Activity-Recognition-with-Smartphones",
+    webapp: "https://github.com/Darshan1233/Human-Activity-Recognition-with-Smartphones",
+    member: [
+      
+    ],
+  },
+  {
+    id: 4,
+    title: "Linkedin-Clone",
+    date: "Dec 2023 - Jan 2024",
+    description:
+      "Created a LinkedIn clone using React and Firebase. Features include user authentication, real-time messaging, dynamic content sorting, and rich text editing. Demonstrates proficiency in front-end development and integration of Firebase for backend services.",
+    image:
+      "https://github.com/Darshan1233/Linkedin-Clone/assets/107185415/37cf6544-6dd7-45f3-8f85-c3bc481bcb05",
+    tags: ["React JS", "FireBase",],
+    category: "web app",
+    github: "https://github.com/Darshan1233/Linkedin-Clone",
+    webapp: "https://github.com/Darshan1233/Linkedin-Clone",
+  },
+  {
+    id: 5,
+    title: "NetFlix-Clone",
+    date: "April 2023 - May 2023",
+    description:
+      "This project involves creating a visually appealing and responsive clone of the Netflix web interface using HTML, CSS, JavaScript, and Bootstrap. The aim is to replicate the look and feel of Netflix's user interface, focusing on the layout, design elements, and responsiveness.",
+    image:
+      "https://github.com/Darshan1233/Netflix-Clone/assets/107185415/06b7d2a4-e8af-4645-8012-3eeabad31236",
+    tags: ["HTML", "JavaScript", "CSS"],
+    category: "web app",
+    github: "https://github.com/Darshan1233/NetFlix-Clone",
+    webapp: "https://darshan1106.github.io/NetFlix-Clone/",
+  },
+  {
+    id: 6,
+    title: "Personal Finance Dashboard",
+    date: "Dec 2023 - Jan 2024",
+    description:
+      "Designed a comprehensive personal finance dashboard to track expenses, income, and financial goals effectively.",
+    image:
+      "https://github.com/Darshan1233/Personal-Finance-Dashboard/assets/107185415/8192d281-5aa0-4d22-99f9-b7dc9d48c78f",
+    tags: ["Power BI"],
+    category: "machine learning",
+    github: "https://github.com/Darshan1233/Personal-Finance-Dashboard",
+    webapp: "https://github.com/Darshan1233/Personal-Finance-Dashboard",
+  },
+];
+
+
